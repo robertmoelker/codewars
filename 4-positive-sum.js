b876b191598b40db3c28e5e0534be1aee49f285c
Robert Moelker <robert@growingminds.nl>
2020-09-29 23:29:13 +0200

    :sparkles: Feature: Positive sum

---
+++ 4-positive-sum.js
@@ -0,0 +1,9 @@
+const positiveSum = (arr) => {
+    return arr.reduce((acc, val) => (val < 0) ? acc : acc + val, 0);
+}
+
+console.log(positiveSum([1,2,3,4,5]),15);
+console.log(positiveSum([1,-2,3,4,5]),13);
+console.log(positiveSum([]),0);
+console.log(positiveSum([-1,-2,-3,-4,-5]),0);
+console.log(positiveSum([-1,2,3,4,-5]),9);
